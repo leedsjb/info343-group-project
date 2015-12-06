3c115e3402eb814bad6653138557960491e538b3
Brian Luu <briluu@uw.edu>
2015-12-06 00:28:57 -0800

    added working ui-routing, navbar with mostly blank pages

--- js/app.js
+++ js/app.js
@@ -1,34 +1,36 @@
-angular.module('143app',['ui.router'])
-.config(function($stateProvider,$urlRouterProvider){
+/*
+
+*/
+
+
+'use strict';
+
+angular.module('143app', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
 
 	// for unfound URLs redirect to home
 	$urlRouterProvider.otherwise("/");
 
-	// $stateProvider
-	// .state('home',{
-	// 	url: '/',
-	// 	templateUrl: 'partials/home.html'
-	// })
-	// .state('orders',{
-	// 	abstract: true, // this state holds child states, not displayed on its own
-	// 	templateUrl: 'partials/orders.html',
-	// })
-	// .state('orders.products',{
-	// 	url: '/orders',
-	// 	templateUrl: 'partials/orders.products.html',
-	// 	controller: 'ProductsCtrl'
-	// })
-	// .state('orders.cart',{
-	// 	url: '/orders/cart',
-	// 	templateUrl: 'partials/orders.cart.html',
-	// 	controller: 'CartCtrl'
-	// })
-	// .state('orders.detail', {
-	// 	url: '/orders/{id}',
-	// 	templateUrl: 'partials/orders.bean-detail.html',
-	// 	controller: 'DetailCtrl'
-	// })
-	
+	$stateProvider
+		.state('home', {
+			url: '/', // root directory
+			templateUrl: 'partials/home.html'
+		})	
+		.state('calendar', {
+			url: '/calendar',
+			templateUrl: 'partials/calendar.html',
+			//controller: 'OrderCtrl'
+		})
+		.state('homework', {
+			url: '/homework',
+			templateUrl: 'partials/homework.html',
+			//controller: 'DetailCtrl'
+		})
+		.state('resources', {
+			url: '/resources',
+			templateUrl: 'partials/resources.html',
+			//controller: 'CartCtrl'
+		})
 })
 
 // For product listing page
